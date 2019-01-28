d07a636b1c426764dec54f1d07a3f2d35c25f2db
Grish <edg.ar.2001@mail.ru>
2019-01-28 22:14:10 +0400

    Commit 3

--- src/index.js
+++ src/index.js
@@ -7,18 +7,25 @@ class Game
                 bullets : 30,
                 level_time : 6000,
                 speed    : 15,
-                bulletsSpeed : 500,
+                bulletsSpeed : 5000,
+                targetSpeed : 10
             },
             level_2 : {
                 id : [2,3],
                 bullets : 30,
                 level_time : 4000,
-                speed    : `30px`,
+                speed    : 30,
                 bulletsTime  : 500
             },
         };
+        this.targetIntervals = {};
         this.position = 0;
+    }
+
+    play (option) {
         this.tankPark();
+        this.targets(option.targetSpeed);
+        this.getTank(option);
     }
 
 
@@ -27,11 +34,33 @@ class Game
         for (let i = 0; i < block.length; i++) {
             block.eq(i).attr(`data-id`,(i+1));
         }
-        this.getTank();
     }
 
-    getTank () {
-        let option = this.options.level_1;
+    moveTarget(elem) {
+        let top = elem.position().top;
+        elem.css('top',(top+0.3)+'px');
+    }
+
+
+
+    targets (speed) {
+        let intervalId = 0,
+            gameZone = $(`.gameZone`),
+            time = Math.ceil(Math.random() * 6500);
+        console.log(time);
+
+        setInterval(()=> {
+            let imgTarget = $(`<img src="images/targets/${Math.ceil(Math.random() * 10)}.png" alt="Target" class="_target" data-intervalId="${intervalId}">`);
+            gameZone.prepend(imgTarget);
+            imgTarget.css(`left`,`${Math.ceil(Math.random() * 90)}%`);
+            this.targetIntervals['interval-'+intervalId] = setInterval(() => {
+                this.moveTarget(imgTarget)
+            },speed);
+            intervalId++;
+        },time);
+    }
+
+    getTank (option) {
         let tankId = option.id;
         let block = $(`.tank[data-id='${tankId}']`);
         let activeTank = block.clone();
@@ -41,27 +70,33 @@ class Game
         let bullets = option.bullets;
 
         for (let i = 0; i < bullets; i++) {
-            let bullet = `<img src="images/bullet.png" class="bullet" alt="bullet" data-img="${i + 1}">`;
+            let bullet = `<img src="images/bullets/bullet.png" class="bullet" alt="bullet" data-img="${i + 1}">`;
             $(`.arsenal`).append(bullet);
         }
     }
 
     shoot () {
-        let positionBullet = 0;
         let bullet = $(`.bullet`);
+        let tank   = $(`.active`);
+        let tankPos = tank.position().left;
         let lastBullet = bullet.eq(bullet.length - 1);
         let activeBullet = lastBullet.clone();
-        activeBullet.addClass(`activeBull`);
+        activeBullet.attr(`src`,`images/bullets/activeBullet.png`);
         $(`.gameZone`).append(activeBullet);
         lastBullet.remove();
-        // for (let i = 0; i < 124 ; i++) {
-        //     activeBullet.css(`transform`,[`translateY(${-(i += 10)}px) rotate(90deg)`]);
-        // }
-        // setInterval(function () {
-        //     console.log(positionBullet += 10);
-        //     activeBullet.css(`transform`,[`translateY(${-(positionBullet += 10)}px) rotate(90deg)`]);
-        // },1200);
+        let activeBulletId = activeBullet.attr(`data-img`);
+        activeBullet.css(`left`,`${tankPos + 27}px`);
+        activeBullet.addClass(`activeBull`).removeClass(`bullet`);
+        let positionBullet = 0;
 
+        if (bullet.length) {
+            let activeBullPos = bullet.position().top;
+            let interval = setInterval(() => {
+                if(activeBullPos !== -1) {
+                    $(`.activeBull[data-img='${activeBulletId}']`).css(`transform`,`translateY(${-(positionBullet += 10)}px)`);
+                }else $(`.activeBull[data-img='${activeBulletId}']`).remove();
+            },50);
+        }
     }
 
     move (keyCode) {
@@ -83,6 +118,11 @@ class Game
 }
 
 let game = new Game();
+
+$(`.play`).click((event) =>{
+    game.play(game.options.level_1);
+    $(event.target).remove();
+});
 
 $(document).on(`keydown`, (e) => {
     let key = e.which;
